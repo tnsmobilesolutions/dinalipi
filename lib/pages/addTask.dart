e9276eaab7f56b9b72d55ce6ae540331921350f1
Manas137 <iamsagar.137@gmail.com>
2021-08-12 09:54:18 +0530

    Add task page 2

--- lib/pages/addTask.dart
+++ lib/pages/addTask.dart
@@ -8,20 +8,93 @@ class AddTaskPage extends StatefulWidget {
 }
 
 class _AddTaskPageState extends State<AddTaskPage> {
+  TimeOfDay time = TimeOfDay.now();
+  TimeOfDay times = TimeOfDay.now();
+
+  void selectStartTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: time,
+    );
+    if (newTime != null) {
+      setState(() {
+        time = newTime;
+      });
+    }
+  }
+
+  void selectEndTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: times,
+    );
+    if (newTime != null) {
+      setState(() {
+        times = newTime;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(
-        child: Scaffold(
-          body: Column(
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Add Task',
-                  hintText: 'Wake Up',
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel')),
+                  ElevatedButton(onPressed: () {}, child: Text('Add')),
+                ],
               ),
+              SizedBox(height: 40.0),
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'ADD TASK ',
+                      labelStyle: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                      hintText: 'Enter Your Task Here',
+                      hintStyle: TextStyle(fontSize: 16.0),
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Start Time'),
+                  TextButton(
+                    child: Icon(Icons.av_timer),
+                    onPressed: selectStartTime,
+                  ),
+                  Text(' ${time.format(context)}')
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('End Time'),
+                  TextButton(
+                    child: Icon(Icons.av_timer),
+                    onPressed: selectEndTime,
+                  ),
+                  Text(' ${times.format(context)}')
+                ],
+              )
             ],
           ),
         ),

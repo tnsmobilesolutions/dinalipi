de40a529159d99bc3e9643765e8820c9a9125353
Barsha Priyadarshini Das <dasbarsha0014@gmail.com>
2023-02-19 21:43:35 +0530

    screen updated

--- lib/presentation/screens/dinalipi/dinalipi_screen.dart
+++ lib/presentation/screens/dinalipi/dinalipi_screen.dart
@@ -6,14 +6,23 @@ import 'package:dinalipi/presentation/screens/dinalipi/dinalipi_addtask.dart';
 import 'package:dinalipi/presentation/screens/dinalipi/dinalipi_points.dart';
 import 'package:dinalipi/presentation/screens/dinalipi/submit_button.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
-class DinalipiScreen extends StatelessWidget {
+class DinalipiScreen extends StatefulWidget {
   const DinalipiScreen({Key? key}) : super(key: key);
 
   @override
+  State<DinalipiScreen> createState() => _DinalipiScreenState();
+}
+
+class _DinalipiScreenState extends State<DinalipiScreen> {
+  final timeController = TextEditingController();
+  final dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay? _startTimeTOD = TimeOfDay.now();
+  @override
   Widget build(BuildContext context) {
-    DateTime selectedDate = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -51,13 +60,242 @@ class DinalipiScreen extends StatelessWidget {
               selectedDate = value;
             },
           ),
-          SizedBox(
-            height: 15,
+          SizedBox(height: 15),
+          TimelineTile(
+            alignment: TimelineAlign.center,
+            lineXY: 1,
+            indicatorStyle: IndicatorStyle(
+              width: 13,
+              indicator: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        width: 5, color: Color.fromARGB(255, 94, 94, 94))),
+              ),
+            ),
+            beforeLineStyle: LineStyle(
+                color: Color.fromARGB(255, 104, 103, 103), thickness: 1.5),
+            isFirst: false,
+            startChild: Padding(
+              padding: const EdgeInsets.only(left: 80),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Color.fromARGB(255, 105, 104, 104),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            endChild: SizedBox(
+              height: 80,
+              width: 100,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 140,
+                      color: Color.fromARGB(255, 220, 239, 253),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Wake Up'),
+                          IconButton(
+                              onPressed: () {
+                                showModalBottomSheet(
+                                  backgroundColor: Colors.transparent,
+                                  context: context,
+                                  builder: (context) {
+                                    return Container(
+                                      height: 300,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(25),
+                                          topRight: Radius.circular(25),
+                                        ),
+                                      ),
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            SizedBox(height: 20),
+                                            Text(
+                                              'Wake up',
+                                              style: TextStyle(fontSize: 20),
+                                            ),
+                                            SizedBox(height: 20),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(10.0),
+                                              child: Column(
+                                                children: [
+                                                  TextFormField(
+                                                    textInputAction:
+                                                        TextInputAction.next,
+                                                    validator: (value) {
+                                                      if (value == null ||
+                                                          value.isEmpty) {
+                                                        return ("Please Enter Correct Date");
+                                                      }
+
+                                                      return null;
+                                                    },
+                                                    onSaved: (value) {
+                                                      dateController.text =
+                                                          value!;
+                                                    },
+                                                    controller: dateController,
+                                                    decoration: InputDecoration(
+                                                      suffixIcon: IconButton(
+                                                          onPressed: () async {
+                                                            DateTime? showDate =
+                                                                await showDatePicker(
+                                                                    context:
+                                                                        context,
+                                                                    initialDate:
+                                                                        DateTime
+                                                                            .now(), //get today's date
+                                                                    firstDate:
+                                                                        DateTime(
+                                                                            2000), //DateTime.now() - not to allow to choose before today.
+                                                                    lastDate:
+                                                                        DateTime(
+                                                                            2101));
+                                                            if (showDate !=
+                                                                null) {
+                                                              String
+                                                                  formattedDate =
+                                                                  DateFormat(
+                                                                          'dd/MM/yyyy')
+                                                                      .format(
+                                                                          showDate);
+                                                              print(
+                                                                  'formated time : $formattedDate');
+                                                              setState(() {
+                                                                dateController
+                                                                        .text =
+                                                                    formattedDate;
+                                                              });
+                                                            }
+                                                          },
+                                                          icon: Icon(
+                                                              Icons.timelapse)),
+                                                      border:
+                                                          OutlineInputBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          15)),
+                                                      labelText:
+                                                          'Choose a date',
+                                                    ),
+                                                  ),
+                                                  SizedBox(height: 20),
+                                                  TextFormField(
+                                                    textInputAction:
+                                                        TextInputAction.next,
+                                                    validator: (value) {
+                                                      if (value == null ||
+                                                          value.isEmpty) {
+                                                        return ("Please Enter Correct Time");
+                                                      }
+
+                                                      return null;
+                                                    },
+                                                    onSaved: (value) {
+                                                      timeController.text =
+                                                          value!;
+                                                    },
+                                                    controller: timeController,
+                                                    decoration: InputDecoration(
+                                                      suffixIcon: IconButton(
+                                                          onPressed: () async {
+                                                            _startTimeTOD =
+                                                                await showTimePicker(
+                                                              context: context,
+                                                              initialTime:
+                                                                  TimeOfDay
+                                                                      .now(),
+                                                            );
+
+                                                            if (_startTimeTOD !=
+                                                                null) {
+                                                              String?
+                                                                  formatedTime =
+                                                                  _startTimeTOD
+                                                                      ?.format(
+                                                                          context);
+                                                              print(
+                                                                  'formated time : $formatedTime');
+                                                              setState(() {
+                                                                timeController
+                                                                        .text =
+                                                                    formatedTime ??
+                                                                        '';
+                                                              });
+                                                            }
+                                                          },
+                                                          icon: Icon(
+                                                              Icons.timelapse)),
+                                                      border:
+                                                          OutlineInputBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          15)),
+                                                      labelText:
+                                                          'Choose a time',
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            SizedBox(height: 10),
+                                            ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Text('Save'))
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                              icon: Icon(Icons.edit)),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      '',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 105, 105, 105),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          timelineTile('2:00 am', ''),
-          timelineTile('3:00 am', ''),
-          timelineTile('4:00 am', ''),
-          timelineTile('5:00 am', ''),
+          SizedBox(height: 15),
+          // timelineTile('2:00 am', ''),
+          // timelineTile('3:00 am', ''),
+          // timelineTile('4:00 am', ''),
+          // timelineTile('5:00 am', ''),
         ],
       ),
       // body: Padding(
--- lib/presentation/screens/report/report_screen.dart
+++ lib/presentation/screens/report/report_screen.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class ReportScreen extends StatelessWidget {
+class ReportScreen extends StatefulWidget {
   const ReportScreen({Key? key}) : super(key: key);
 
   @override
+  State<ReportScreen> createState() => _ReportScreenState();
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+  final dateController = TextEditingController();
+  List<BottomNavigationBarItem> item = [];
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Report Content Here'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 247, 237, 222),
+        title: Text(
+          'Dinacharya',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+        ),
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(
+              'Show Month',
+              style: TextStyle(fontSize: 20),
+            ),
+            TextFormField(
+              textInputAction: TextInputAction.next,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return ("Please Enter Correct Month");
+                }
+
+                return null;
+              },
+              onSaved: (value) {
+                dateController.text = value!;
+              },
+              controller: dateController,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    onPressed: () async {
+                      DateTime? showDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(), //get today's date
+                          firstDate: DateTime(
+                              2023), //DateTime.now() - not to allow to choose before today.
+                          lastDate: DateTime(2100));
+                      if (showDate != null) {
+                        String formattedDate =
+                            DateFormat.yMMMM().format(showDate);
+                        print('formated time : $formattedDate');
+                        setState(() {
+                          dateController.text = formattedDate;
+                        });
+                      }
+                    },
+                    icon: Icon(Icons.timelapse)),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                labelText: 'Choose a Month',
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Generate')),
+          ],
+        ),
+      )),
+      // bottomNavigationBar: BottomNavigationBar(items: ),
     );
   }
 }
